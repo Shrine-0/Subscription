1139d738ab5c600c4e5d9977a3088e5054149d33
Shrine <nischalthapa10@hotmail.com>
2023-07-04 13:35:24 +0545

    order request api made

--- order/urls.py
+++ order/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("webhook/",views.stripe_webhook,name="webhook"),
     path("get-all/",views.get_order,name = "get-all"),
     path("add-new/",views.add_order,name = "add-new"),
     path("<int:pk>/update/",views.process_order,name ="update"),
--- order/views.py
+++ order/views.py
@@ -9,8 +9,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from product.models import Product
 import stripe
+from subscription.settings import STRIPE_PRIVATE_KEY, STRIPE_WEBHOOK_SECERET
 from utils.helpers import get_current_host
-
+from django.contrib.auth.models import User
 @api_view(["GET"])
 def get_order(request):
     order = Order.objects.all()
@@ -65,24 +66,25 @@ def delete_order(request,pk):
     return Response({"order-details":f"order for id :{pk} is deleted"})
 
 
-stripe.api_key = os.getenv("STRIPE_PRIVATE_KEY")
-
+stripe.api_key = STRIPE_PRIVATE_KEY
 @api_view(["POST"])
-# @permission_classes([IsAdminUser]) #! --- authentiaction
+@permission_classes([IsAdminUser]) #! --- authentiaction
 def create_checkout_session(request):
     YOUR_DOMAIN = get_current_host(request)
 
-    # user = request.user #! --- authentication
+    user = request.user #! --- authentication
     data = request.data
     
     orders = data["order"]
     
-    
+    user_details ={
+        "user":user.id
+    }
     checkout_order_items=[]
     for i in orders:
         checkout_order_items.append({
             "price_data":{
-                "currency" : "usd",
+                "currency" : "npr",
                 "product_data" : {
                     "name":i["name"],
                     # "images": 
@@ -91,17 +93,63 @@ def create_checkout_session(request):
                 "recurring":{
                     "interval":"year"
                 },
-                "unit_amount":i["price"]
+                "unit_amount":i["price"]*100
             },
             "quantity":i["quantity"]
         })
     checkout_session = stripe.checkout.Session.create(
         payment_method_types = ['card'],
+        metadata = user_details,
         line_items = checkout_order_items,
-        # customer_email = user.email,
+        customer_email = user.email,
         mode = 'subscription',
         subscription_data = {'trial_period_days':7,},
         success_url = YOUR_DOMAIN,
         cancel_url = YOUR_DOMAIN,
+        # idempotency_key="keeee"
     )
     return Response({"session":checkout_session})
+
+@api_view(["POST"])
+def stripe_webhook(request):
+    
+    webhook_secret = "whsec_599dbf3f66bece9e8b11c8c2c8bc3927fa67d82a15bf76a8fc1b85b70652aff0"
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+    
+    try:
+        event = stripe.Webhook.construct_event(
+            payload,sig_header,webhook_secret
+        )
+        
+    except ValueError as e:
+        return Response({"error":"Invalid Payload - ValueError"},status = status.HTTP_400_BAD_REQUEST)
+    
+    except stripe.error.SignatureVerificationError as e:
+        return Response({"error":"Invalid Signature - SignatureVerificationError"},status = status.HTTP_400_BAD_REQUEST)
+    
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        
+        line_items = stripe.checkout.Session.list_line_items(session['id'])
+        
+        price = session['amount_total']
+        
+        
+        for item in line_items['data']:
+            print(f'item : {item}')
+            line_product =stripe.Product.retrieve(item.price.product)
+            product_id = line_product.metadata.product_id
+            product = Product.objects.get(id = product_id)
+            item = Order.objects.create(  
+            user = User(session.metadata.user),
+            total_amount = price,
+            payment_mode = "Card",
+            payment_status = "Paid",
+            )
+
+        return Response({'details':'Payment successful'})
+    else:
+        return Response({"error": f"Unhandled event type: {event['type']}"}, status=status.HTTP_400_BAD_REQUEST)
+
